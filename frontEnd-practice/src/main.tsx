5a483501de9a2c6833f1630ef4b36ee0de261e83
leitogonzales <leitogonzalescueva@gmail.com>
2024-06-26 19:20:02 -0500

    Se añadieron cambios al proyecto

--- frontEnd-practice/src/main.tsx
+++ frontEnd-practice/src/main.tsx
@@ -6,7 +6,7 @@ import Search from "./components/Search.tsx";
 import Category from "./components/category/Category.tsx";
 import Footer from "./components/footer/Footer.tsx";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Header />
     <Search />
